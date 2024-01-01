0014c2e1cb1a7caf02d3eb251c42e863b617b52c
coup3z-pixel <seanmclennanqa@gmail.com>
2024-01-01 13:19:17 +1000

    Adding Full Body Menu

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 function showMenu() {
 	const menuDiv = document.getElementById('menu-div');
-	console.log("HI");
-	menuDiv.style.backdropFilter = "blur(5px)";
-	menuDiv.style.backgroundColor = "rgba(255,255,255,0.5)";
 	menuDiv.style.visibility = "visible";
+	menuDiv.style.opacity = "0.75";
+	menuDiv.style.backdropFilter = "blur(200px)";
+}
+
+function hideMenu() {
+	const menuDiv = document.getElementById('menu-div');
+	menuDiv.style.backdropFilter = "blur(0px)";
+	menuDiv.style.opacity = "0";
+	menuDiv.style.visibility = "hidden";
 }
